3708e5df2fe6e35d8fc579a3e6c0d6cab3821b62
Kaffi Y. <gh@109cafe.com>
2025-01-03 17:19:13 +0800

    POC done (#1)

--- src/chef-api-fs-provider.ts
+++ src/chef-api-fs-provider.ts
@@ -3,23 +3,86 @@ import * as vscode from "vscode";
 import Trie from "wayfarer/trie";
 import { TempClient } from "./temp-client";
 const trie = new Trie();
-trie.create("/organizations/:org/nodes/:nodeName");
+// todo use openapi spec
+interface RestTrieNode extends TrieNode {
+  metadata?: {
+    plain: boolean;
+  };
+}
+function createNode(route: string, metadata?: Partial<RestTrieNode["metadata"]>): RestTrieNode {
+  const node = trie.create(route) as RestTrieNode;
+  node.metadata = { plain: false, ...metadata };
+  return node;
+}
+createNode("/organizations/:org");
+createNode("/organizations/:org/clients");
+createNode("/organizations/:org/clients/:client");
+createNode("/organizations/:org/clients/:client/keys");
+createNode("/organizations/:org/clients/:client/keys/:key");
+createNode("/organizations/:org/containers");
+createNode("/organizations/:org/containers/:container");
+createNode("/organizations/:org/cookbook_artifacts");
+createNode("/organizations/:org/cookbook_artifacts/:cookbook_artifact");
+createNode("/organizations/:org/cookbook_artifacts/:cookbook_artifact/:id");
+createNode("/organizations/:org/cookbooks");
+createNode("/organizations/:org/cookbooks/_latest");
+createNode("/organizations/:org/cookbooks/_recipes");
+createNode("/organizations/:org/cookbooks/:cookbook");
+createNode("/organizations/:org/cookbooks/:cookbook/version");
+createNode("/organizations/:org/data");
+createNode("/organizations/:org/data/:data");
+createNode("/organizations/:org/data/:data/:item");
+createNode("/organizations/:org/environments");
+createNode("/organizations/:org/environments/_default");
+createNode("/organizations/:org/environments/:environment");
+createNode("/organizations/:org/environments/:environment/cookbooks");
+createNode("/organizations/:org/environments/:environment/cookbooks/:cookbook");
+createNode("/organizations/:org/environments/:environment/cookbooks_versions");
+createNode("/organizations/:org/environments/:environment/nodes");
+createNode("/organizations/:org/environments/:environment/recipes");
+createNode("/organizations/:org/environments/:environment/roles/:role");
+createNode("/organizations/:org/groups");
+createNode("/organizations/:org/groups/:group");
+createNode("/organizations/:org/nodes");
+createNode("/organizations/:org/nodes/:node");
+createNode("/organizations/:org/policies");
+createNode("/organizations/:org/policy_groups");
+createNode("/organizations/:org/policy_groups/:policy_group");
+createNode("/organizations/:org/policies/:policy");
+createNode("/organizations/:org/required_recipe", { plain: true });
+createNode("/organizations/:org/roles");
+createNode("/organizations/:org/roles/:role");
+createNode("/organizations/:org/roles/:role/environments");
+createNode("/organizations/:org/roles/:role/environments/:environment");
+createNode("/organizations/:org/sandboxes");
+createNode("/organizations/:org/sandboxes/:sandbox");
+createNode("/organizations/:org/universe");
+createNode("/organizations/:org/users");
+createNode("/organizations/:org/users/:user");
+interface Route extends RestTrieNode {
+  path: string;
+}
+function getNode(path: string): Route | undefined {
+  const n = trie.match(path) as Route;
+  if (!n) {
+    return n;
+  }
+  n.path = path;
+  return n;
+}
 
 export class File implements vscode.FileStat {
   type: vscode.FileType;
   ctime: number;
   mtime: number;
   size: number;
+  permissions?: vscode.FilePermission;
 
-  name: string;
-  data?: Uint8Array;
-
-  constructor(name: string) {
+  constructor() {
     this.type = vscode.FileType.File;
     this.ctime = Date.now();
     this.mtime = Date.now();
     this.size = 0;
-    this.name = name;
   }
 }
 
@@ -28,15 +91,13 @@ export class Directory implements vscode.FileStat {
   ctime: number;
   mtime: number;
   size: number;
+  permissions?: vscode.FilePermission;
 
-  name: string;
-
-  constructor(name: string) {
+  constructor() {
     this.type = vscode.FileType.Directory;
     this.ctime = Date.now();
     this.mtime = Date.now();
     this.size = 0;
-    this.name = name;
   }
 }
 
@@ -47,30 +108,95 @@ export class ChefApiFsProvider implements vscode.FileSystemProvider {
 
   // --- manage file metadata
 
-  stat(uri: vscode.Uri): PromiseLike<vscode.FileStat> {
-    console.log("stat", uri.path);
-    return this._lookup(uri, false);
+  stat(uri: vscode.Uri): vscode.FileStat {
+    const basename = path.basename(uri.path);
+    // todo from node
+    const root = basename.startsWith(".root");
+    const s = this._lookup(uri, false);
+    // todo: fetch metadata from server
+    if (!root && Object.keys(s.nodes).length > 0) {
+      const entry = new Directory();
+      entry.permissions = s.nodes.$$ ? undefined : vscode.FilePermission.Readonly;
+      return entry;
+    } else {
+      const entry = new File();
+      entry.permissions = root ? vscode.FilePermission.Readonly : undefined;
+      return entry;
+    }
   }
 
-  readDirectory(uri: vscode.Uri): [string, vscode.FileType][] {
-    console.log("readDirectory");
-    const entry = this._lookupAsDirectory(uri, false);
-    const result: [string, vscode.FileType][] = [];
-    for (const [name, child] of entry.entries) {
-      result.push([name, child.type]);
+  async readDirectory(uri: vscode.Uri): Promise<[string, vscode.FileType][]> {
+    console.log("readDirectory", uri.path);
+
+    const node = this._lookupAsDirectory(uri);
+
+    const result: [string, vscode.FileType][] = [[".root.json", vscode.FileType.File]];
+    for (const [name, child] of Object.entries(node.nodes)) {
+      if (name === "$$") {
+        // todo read children
+        const res = await this.client.get(node.path);
+        console.log("readChildren", uri.path, res.status, res.url);
+        if (res.status === 200) {
+          const r = (await res.json()) as
+            | Record<string, string>
+            | Record<string, { uri: string; [key: string]: any }>
+            | { uri: string; name: string; [key: string]: any }[];
+          let rr: { uri: string; name: string; [key: string]: any }[] = [];
+          if (Array.isArray(r)) {
+            rr = r;
+          } else {
+            rr = Object.entries(r).map(([name, v]) => {
+              if (typeof v === "string") {
+                return { uri: v, name };
+              }
+              return { uri: v.uri, name, ...v };
+            });
+          }
+          for (const n of rr) {
+            try {
+              console.log(uri.with({ path: uri.path + "/" + n.name }));
+              const c = this.stat(uri.with({ path: uri.path + "/" + n.name }));
+              result.push([n.name, c.type]);
+            } catch {}
+          }
+        }
+        continue;
+      }
+      if (Object.keys(child.nodes).length > 0) {
+        result.push([name, vscode.FileType.Directory]);
+      } else {
+        result.push([name, vscode.FileType.File]);
+      }
     }
+    console.log("readDirectory", uri.path, Object.fromEntries(result));
     return result;
   }
 
   // --- manage file contents
 
-  readFile(uri: vscode.Uri): Uint8Array {
-    console.log("readFile");
-    const data = this._lookupAsFile(uri, false).data;
-    if (data) {
-      return data;
+  async readFile(uri: vscode.Uri): Promise<Uint8Array> {
+    const node = this._lookupAsFile(uri);
+    const res = await this.client.get(node.path, { plain: node.metadata?.plain });
+    console.log("readFile", uri.path, res.status, res.url);
+    if (res.status === 404) {
+      throw vscode.FileSystemError.FileNotFound(uri);
     }
-    throw vscode.FileSystemError.FileNotFound();
+    if (res.status === 403) {
+      throw vscode.FileSystemError.NoPermissions(uri);
+    }
+    if (res.status === 401) {
+      throw vscode.FileSystemError.NoPermissions(uri);
+    }
+    if (res.status > 499) {
+      throw vscode.FileSystemError.Unavailable(uri);
+    }
+    const buf = new Uint8Array(await res.arrayBuffer());
+    try {
+      const r = JSON.parse(new TextDecoder().decode(buf));
+      console.log("read file success", r);
+      return new TextEncoder().encode(JSON.stringify(r, null, 2));
+    } catch {}
+    return buf;
   }
 
   writeFile(
@@ -158,10 +284,13 @@ export class ChefApiFsProvider implements vscode.FileSystemProvider {
 
   // --- lookup
 
-  private async _lookup(uri: vscode.Uri, silent: false): Promise<Entry>;
-  private async _lookup(uri: vscode.Uri, silent: boolean): Promise<Entry | undefined>;
-  private async _lookup(uri: vscode.Uri, silent: boolean): Promise<Entry | undefined> {
-    const m = trie.match(uri.path);
+  private _lookup(uri: vscode.Uri, silent: false): Route;
+  private _lookup(uri: vscode.Uri, silent: boolean): Route;
+  private _lookup(uri: vscode.Uri, silent: boolean): Route | undefined {
+    const basename = path.basename(uri.path);
+    const isRoot = basename.startsWith(".root");
+    const parent = getNode(path.dirname(uri.path));
+    const m = isRoot ? parent : getNode(uri.path);
     if (!m) {
       if (silent) {
         return undefined;
@@ -169,34 +298,19 @@ export class ChefApiFsProvider implements vscode.FileSystemProvider {
         throw vscode.FileSystemError.FileNotFound(uri);
       }
     }
-    const { nodes } = m;
-    const basename = path.basename(uri.path);
-    if (Object.keys(nodes).length === 0) {
-      return new File(basename);
-    } else {
-      return new Directory(basename);
-    }
+    return m;
   }
 
-  private _lookupAsDirectory(uri: vscode.Uri, silent: boolean): Directory {
-    const entry = this._lookup(uri, silent);
-    if (entry instanceof Directory) {
-      return entry;
+  private _lookupAsDirectory(uri: vscode.Uri): Route {
+    const m = this._lookup(uri, false);
+    if (Object.keys(m.nodes).length === 0) {
+      throw vscode.FileSystemError.FileNotADirectory(uri);
     }
-    throw vscode.FileSystemError.FileNotADirectory(uri);
+    return m;
   }
 
-  private _lookupAsFile(uri: vscode.Uri, silent: boolean): File {
-    const entry = this._lookup(uri, silent);
-    if (entry instanceof File) {
-      return entry;
-    }
-    throw vscode.FileSystemError.FileIsADirectory(uri);
-  }
-
-  private _lookupParentDirectory(uri: vscode.Uri): Directory {
-    const dirname = uri.with({ path: path.posix.dirname(uri.path) });
-    return this._lookupAsDirectory(dirname, false);
+  private _lookupAsFile(uri: vscode.Uri): Route {
+    return this._lookup(uri, false);
   }
 
   // --- manage file events
--- src/temp-client.ts
+++ src/temp-client.ts
@@ -4,7 +4,7 @@ import { TmpConfig } from "./tmp";
 
 export class TempClient {
   private key = importKey(readFileSync(TmpConfig.key, "utf8"));
-  async get(path: string) {
+  async get(path: string, { plain = false }: { plain?: boolean } = {}) {
     const url = new URL(path, TmpConfig.server).toString();
     const method = "GET";
     console.log("GET", url);
@@ -14,12 +14,15 @@ export class TempClient {
       path: url,
       userId: TmpConfig.nodeName,
     });
-    const headers = {
+    const headers: Record<string, string> = {
       ...signedHeaders,
       "content-type": "application/json",
       accept: "application/json",
       "X-Chef-Version": "18.6.2",
     };
+    if (plain) {
+      delete headers.accept;
+    }
     console.log(headers);
 
     const res = await fetch(url, { method, headers });
